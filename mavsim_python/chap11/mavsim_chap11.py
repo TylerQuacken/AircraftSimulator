89bc90c47f0853152add18ce296fe63f5c0ebf0d
Tyler Quackenbush <tylerquacken@gmail.com>
2021-04-01 15:03:15 -0600

    Almost works for fillets

--- mavsim_python/chap11/mavsim_chap11.py
+++ mavsim_python/chap11/mavsim_chap11.py
@@ -19,6 +19,7 @@ from chap8.observer import Observer
 from chap10.path_follower import PathFollower
 from chap11.path_manager import PathManager
 from chap11.waypoint_viewer import WaypointViewer
+from IPython import embed
 
 # initialize the visualization
 VIDEO = False  # True==write video, False==don't write video
@@ -34,22 +35,22 @@ if VIDEO is True:
 wind = WindSimulation(SIM.ts_simulation)
 mav = MavDynamics(SIM.ts_simulation)
 autopilot = Autopilot(SIM.ts_simulation)
-observer = Observer(SIM.ts_simulation)
+observer = Observer(SIM.ts_simulation, mav.true_state)
 path_follower = PathFollower()
 path_manager = PathManager()
 
 # waypoint definition
 from message_types.msg_waypoints import MsgWaypoints
 waypoints = MsgWaypoints()
-waypoints.type = 'straight_line'
-#waypoints.type = 'fillet'
+# waypoints.type = 'straight_line'
+waypoints.type = 'fillet'
 #waypoints.type = 'dubins'
 Va = PLAN.Va0
 waypoints.add(np.array([[0, 0, -100]]).T, Va, np.radians(0), np.inf, 0, 0)
 waypoints.add(np.array([[1000, 0, -100]]).T, Va, np.radians(45), np.inf, 0, 0)
 waypoints.add(np.array([[0, 1000, -100]]).T, Va, np.radians(45), np.inf, 0, 0)
-waypoints.add(np.array([[1000, 1000, -100]]).T, Va, np.radians(-135), np.inf, 0, 0)
-
+waypoints.add(
+    np.array([[1000, 1000, -100]]).T, Va, np.radians(-135), np.inf, 0, 0)
 
 # initialize the simulation time
 sim_time = SIM.start_time
@@ -60,8 +61,8 @@ print("Press Command-Q to exit...")
 while sim_time < SIM.end_time:
     # -------observer-------------
     measurements = mav.sensors()  # get sensor measurements
-    estimated_state = observer.update(measurements)  # estimate states from measurements
-
+    # estimated_state = observer.update(measurements)  # estimate states from measurements
+    estimated_state = mav.true_state
     # -------path manager-------------
     path = path_manager.update(waypoints, PLAN.R_min, estimated_state)
 
@@ -69,7 +70,8 @@ while sim_time < SIM.end_time:
     autopilot_commands = path_follower.update(path, estimated_state)
 
     # -------autopilot-------------
-    delta, commanded_state = autopilot.update(autopilot_commands, estimated_state)
+    delta, commanded_state = autopilot.update(autopilot_commands,
+                                              estimated_state)
 
     # -------physical system-------------
     current_wind = wind.update()  # get the new wind vector
@@ -77,12 +79,14 @@ while sim_time < SIM.end_time:
 
     # -------update viewer-------------
     if plot_timer > SIM.ts_plotting:
-        waypoint_view.update(mav.true_state, path, waypoints)  # plot path and MAV
-        data_view.update(mav.true_state,  # true states
-                         estimated_state,  # estimated states
-                         commanded_state,  # commanded states
-                         delta,  # input to aircraft
-                         SIM.ts_plotting)
+        waypoint_view.update(mav.true_state, path,
+                             waypoints)  # plot path and MAV
+        data_view.update(
+            mav.true_state,  # true states
+            estimated_state,  # estimated states
+            commanded_state,  # commanded states
+            delta,  # input to aircraft
+            SIM.ts_plotting)
         plot_timer = 0
     plot_timer += SIM.ts_simulation
 
@@ -94,7 +98,3 @@ while sim_time < SIM.end_time:
 
 if VIDEO is True:
     video.close()
-
-
-
-
--- mavsim_python/chap11/path_manager.py
+++ mavsim_python/chap11/path_manager.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 sys.path.append('..')
-from chap11.dubins_parameters import DubinsParameters
+# from chap11.dubins_parameters import DubinsParameters
 from message_types.msg_path import MsgPath
 
 
@@ -14,12 +14,12 @@ class PathManager:
         self.ptr_current = 1
         self.ptr_next = 2
         self.num_waypoints = 0
-        self.halfspace_n = np.inf * np.ones((3,1))
-        self.halfspace_r = np.inf * np.ones((3,1))
+        self.halfspace_n = np.inf * np.ones((3, 1))
+        self.halfspace_r = np.inf * np.ones((3, 1))
         # state of the manager state machine
         self.manager_state = 1
         self.manager_requests_waypoints = True
-        self.dubins_path = DubinsParameters()
+        # self.dubins_path = DubinsParameters()
 
     def update(self, waypoints, radius, state):
         if waypoints.num_waypoints == 0:
@@ -32,91 +32,192 @@ class PathManager:
         elif waypoints.type == 'fillet':
             self.fillet_manager(waypoints, radius, state)
         elif waypoints.type == 'dubins':
-            self.dubins_manager(waypoints, radius, state)
+            # self.dubins_manager(waypoints, radius, state)
+            raise NotImplementedError
         else:
             print('Error in Path Manager: Undefined waypoint type.')
         return self.path
 
+    def line_manager(self, waypoints, state):
+        mav_pos = np.array([[state.north, state.east, -state.altitude]]).T
+        # if the waypoints have changed, update the waypoint pointer
+        if waypoints.flag_waypoints_changed is True:
+            waypoints.flag_manager_requests_waypoints = False
+            waypoints.flag_waypoints_changed = False
+            self.num_waypoints = waypoints.num_waypoints
+            self.initialize_pointers()
+            self.construct_line(waypoints)
+
+        # state machine for line path
+        # if its in the half space
+        if self.inHalfSpace(mav_pos):
+            self.increment_pointers()
+            self.construct_line(waypoints)
+            if self.ptr_current == 0:
+                self.manager_requests_waypoints = True
+
     def initialize_pointers(self):
         if self.num_waypoints >= 3:
-            self.ptr_previous = 
-            self.ptr_current = 
-            self.ptr_next = 
+            self.ptr_previous = 0
+            self.ptr_current = 1
+            self.ptr_next = 2
         else:
             print('Error Path Manager: need at least three waypoints')
 
     def increment_pointers(self):
-        self.ptr_previous = 
-        self.ptr_current = 
-        self.ptr_next = 
+        self.ptr_previous = self.ptr_current
+        self.ptr_current = self.ptr_next
+        # make next or current 9999 if invalid
+        self.ptr_next = self.ptr_next + 1
+        if self.ptr_next > self.num_waypoints - 1:
+            self.ptr_next = 9999
+        if self.ptr_current > self.num_waypoints - 1:
+            self.ptr_current = 9999
 
     def inHalfSpace(self, pos):
-        if (): #implement code here
+        if (pos - self.halfspace_r).T @ self.halfspace_n >= 0:
             return True
         else:
             return False
 
-    def line_manager(self, waypoints, state):
-        mav_pos = np.array([[state.north, state.east, -state.altitude]]).T
-        # if the waypoints have changed, update the waypoint pointer
-
-        # state machine for line path
-
     def construct_line(self, waypoints):
-        previous = waypoints.ned[:, self.ptr_previous:self.ptr_previous+1]
+        previous = waypoints.ned[:, self.ptr_previous:self.ptr_previous + 1]
         if self.ptr_current == 9999:
-            current = 
+            # use path.line_direction to fly straight
+            current = previous + 100 * self.path.line_direction
         else:
-            current = 
+            # get current waypoint from waypoints.ned
+            current = waypoints.ned[:, self.ptr_current].reshape(-1, 1)
         if self.ptr_next == 9999:
-            next = 
+            # go straight for even longer
+            next = previous + 200 * self.path.line_direction
         else:
-            next = 
+            next = waypoints.ned[:, self.ptr_next].reshape(-1, 1)
         #update path variables
+        self.path.type = 'line'
+        self.path.airspeed = waypoints.airspeed.item(self.ptr_current)
+        self.path.line_origin = previous
+        qPrev = current - previous
+        qPrev /= np.linalg.norm(qPrev)
+        self.path.line_direction = qPrev
+        qNext = next - current
+        qNext /= np.linalg.norm(qNext)
+
+        self.halfspace_n = qPrev + qNext
+        self.halfspace_n /= np.linalg.norm(self.halfspace_n)
+        self.halfspace_r = current
+        self.path.plot_updated = False
 
     def fillet_manager(self, waypoints, radius, state):
         mav_pos = np.array([[state.north, state.east, -state.altitude]]).T
         # if the waypoints have changed, update the waypoint pointer
+        if waypoints.flag_waypoints_changed is True:
+            # waypoints.flag_manager_requests_waypoints = False
+            waypoints.flag_waypoints_changed = False
+            self.num_waypoints = waypoints.num_waypoints
+            self.initialize_pointers()
+            self.construct_fillet_line(waypoints, radius)
+            self.manager_state = 1
 
         # state machine for fillet path
+        if self.manager_state == 1:
+            # follow straight line path from previous to current
+            if self.inHalfSpace(mav_pos):
+                # entered into H1
+                self.construct_fillet_circle(waypoints, radius)
+                self.manager_state == 3
+        elif self.manager_state == 2:
+            # follow orbit until out of H2
+            if not self.inHalfSpace(mav_pos):
+                self.manager_state = 3
+        elif self.manager_state == 3:
+            # follow orbit from previous->current to current->next
+            if self.inHalfSpace(mav_pos):
+                # entered into half plane H2
+                self.increment_pointers()
+                self.construct_fillet_line(waypoints, radius)
+                self.manager_state = 1
+                # requests new waypoints
+                if self.ptr_current == 0:
+                    self.manager_requests_waypoints = True
 
     def construct_fillet_line(self, waypoints, radius):
-        previous = waypoints.ned[:, self.ptr_previous:self.ptr_previous+1]
+        previous = waypoints.ned[:, self.ptr_previous:self.ptr_previous + 1]
         if self.ptr_current == 9999:
-            current = 
+            current = previous + 100 * self.path.line_direction
         else:
-            current = 
+            current = waypoints.ned[:, self.ptr_current].reshape(-1, 1)
         if self.ptr_next == 9999:
-            next = 
+            next = previous + 200 * self.path.line_direction
         else:
-            next = 
-        #update path variables
-       
+            next = waypoints.ned[:, self.ptr_next].reshape(-1, 1)
+        # update path variables same
+        self.path.type = 'line'
+        self.path.airspeed = waypoints.airspeed.item(self.ptr_current)
+        self.path.line_origin = previous
+        qPrev = current - previous
+        qPrev /= np.linalg.norm(qPrev)
+        self.path.line_direction = qPrev
+        qNext = next - current
+        qNext /= np.linalg.norm(qNext)
+        varphi = np.arccos(-qPrev.T @ qNext)
+
+        self.halfspace_n = qPrev
+        self.halfspace_r = current - (radius / np.tan(varphi / 2)) * qPrev
+        self.path.plot_updated = False
+
+        # halfspace_n is just previous
+        # halfspace_r should be current minus a bit
 
     def construct_fillet_circle(self, waypoints, radius):
-        previous = waypoints.ned[:, self.ptr_previous:self.ptr_previous+1]
+        previous = waypoints.ned[:, self.ptr_previous:self.ptr_previous + 1]
         if self.ptr_current == 9999:
-            current = 
+            current = previous + 100 * self.path.line_direction
         else:
-            current = 
+            current = waypoints.ned[:, self.ptr_current].reshape(-1, 1)
         if self.ptr_next == 9999:
-            next = 
+            next = previous + 200 * self.path.line_direction
         else:
-            next = 
-         #update path variables
-
-    def dubins_manager(self, waypoints, radius, state):
-        mav_pos = np.array([[state.north, state.east, -state.altitude]]).T
-        # if the waypoints have changed, update the waypoint pointer
-
-        # state machine for dubins path
-
-    def construct_dubins_circle_start(self, waypoints, dubins_path):
+            next = waypoints.ned[:, self.ptr_next].reshape(-1, 1)
         #update path variables
+        self.path.type = 'orbit'
+        self.path.airspeed = waypoints.airspeed.item(self.ptr_current)
+        qPrev = current - previous
+        qPrev /= np.linalg.norm(qPrev)
+        qNext = next - current
+        qNext /= np.linalg.norm(qNext)
+        varphi = np.arccos(-qPrev.T @ qNext)
+        # set orbit_center
+        qC = qPrev - qNext
+        qC /= np.linalg.norm(qC)
+        self.path.orbit_center = current - (radius / np.sin(varphi / 2)) * qC
+        self.path.orbit_radius = radius
+        # set orbit direction as CW (lam=1) or CCW (lam=2)
+        if np.sign(
+                qPrev.item(0) * qNext.item(1) -
+                qPrev.item(1) * qNext.item(0)) > 0:
+            self.path.orbit_direction = 'CW'
+        else:
+            self.path.orbit_direction = 'CCW'
 
-    def construct_dubins_line(self, waypoints, dubins_path):
-        #update path variables
+        self.halfspace_n = qNext
+        self.halfspace_r = current + (radius / np.tan(varphi / 2)) * qNext
+        self.path.plot_updated = False
 
-    def construct_dubins_circle_end(self, waypoints, dubins_path):
-        #update path variables
+    # def dubins_manager(self, waypoints, radius, state):
+    #     mav_pos = np.array([[state.north, state.east, -state.altitude]]).T
+    #     # if the waypoints have changed, update the waypoint pointer
 
+    #     # state machine for dubins path
+
+    # def construct_dubins_circle_start(self, waypoints, dubins_path):
+    #     #update path variables
+    #     pass
+
+    # def construct_dubins_line(self, waypoints, dubins_path):
+    #     #update path variables
+    #     pass
+
+    # def construct_dubins_circle_end(self, waypoints, dubins_path):
+    #     #update path variables
+    #     pass
